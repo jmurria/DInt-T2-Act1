ce8117728b70a40aed679949bcae12146f33b5cc
julia.murria <julia.murria@iesdoctorbalmis.com>
2020-10-26 21:17:58 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Act2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int NumeroAAdivinar;
+        private void ComprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            int numeroEntrado = int.Parse(entradaTextBox.Text);
+
+            if (NumeroAAdivinar > numeroEntrado)
+            {
+                resultadoTextBlock.Text = "¡Te has quedado corto!";
+            }
+            else if (NumeroAAdivinar < numeroEntrado)
+            {
+                resultadoTextBlock.Text = "¡Te has pasado!";
+            }
+            else if (NumeroAAdivinar == numeroEntrado)
+            {
+                resultadoTextBlock.Text = "A la primera y sin fallos.";
+            }
+        }
+
+        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultadoTextBlock.Text = null;
+            entradaTextBox.Text = null;
+        }
+
+        private void SetNumeroAAdivinar()
+        {
+            Random seed = new Random();
+            NumeroAAdivinar = seed.Next(0, 101);
+        }
+
+        public MainWindow()
+        {
+            SetNumeroAAdivinar();
+            InitializeComponent();
+        }
+    }
+}
